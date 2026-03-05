ba154ba80db23e15318141775b4d1e7f6f970475
fjschluss <fschlusselblum@gmail.com>
2026-03-05 18:58:43 -0300

    feat: implementar creación de producto y guardado en archivo JSON

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,6 +5,7 @@ const productController = require("../controllers/productController");
 
 router.get("/", productController.list);
 router.get("/create", productController.create);
+router.post("/", productController.store);
 router.get("/:id", productController.detail);
 router.get("/:id/edit", productController.edit);
 
