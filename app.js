76f4638c9dec569c5368807d80b541b2c194fc44
fjschluss <fschlusselblum@gmail.com>
2026-03-05 19:08:58 -0300

    feat: agregar formulario de edición de producto con datos precargados

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var methodOverride = require("method-override");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
