69c8659946db160c8aaef4cf0d08feee35dafb2a
fjschluss <fschlusselblum@gmail.com>
2026-03-08 10:31:26 -0300

    refactor: mover secret de sesión a variable de entorno

--- app.js
+++ app.js
@@ -3,57 +3,54 @@ require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var methodOverride = require("method-override");
+var methodOverride = require('method-override');
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
-var userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
 var logger = require('morgan');
 
-// Importar rutas
-var mainRoutes = require("./routes/mainRoutes");
-var productRoutes = require("./routes/productRoutes");
-var userRoutes = require("./routes/userRoutes");
+var userLoggedMiddleware = require('./middlewares/userLoggedMiddleware');
+
+var mainRoutes = require('./routes/mainRoutes');
+var productRoutes = require('./routes/productRoutes');
+var userRoutes = require('./routes/userRoutes');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 app.use(cookieParser());
+
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'lubo_fallback_secret',
   resave: false,
   saveUninitialized: false
 }));
+
 app.use(userLoggedMiddleware);
+
 app.use((req, res, next) => {
-    res.locals.userLogged = req.session.userLogged;
-    next();
+  res.locals.userLogged = req.session.userLogged || null;
+  next();
 });
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Usar rutas
-app.use("/", mainRoutes);
-app.use("/products", productRoutes);
-app.use("/users", userRoutes);
+app.use('/', mainRoutes);
+app.use('/products', productRoutes);
+app.use('/users', userRoutes);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
