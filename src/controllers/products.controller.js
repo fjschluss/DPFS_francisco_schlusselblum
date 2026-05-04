7de2df32df80b710455da59d11ed4cbd1dbda9e6
fjschluss <fschlusselblum@gmail.com>
2026-05-03 23:41:15 -0300

    refactor: reorganizar vistas en src/views/products y src/views/users

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,10 +1,20 @@
-const productsController = {  
-    list: (req, res) => res.send('list - próximamente EJS'),
-    detail: (req, res) => res.send('detail - próximamente EJS'),
-    create: (req, res) => res.send('create - próximamente EJS'),
-    createForm: (req, res) => res.send('create - próximamente EJS'),
-    edit: (req, res) => res.send('edit - próximamente EJS'),
-    editForm: (req, res) => res.send('edit - próximamente EJS'),
+const products = require('../../data/products');
+
+const productsController = {
+    list:       (req, res) => res.render('products/list',   { title: 'Productos – LuBo', products }),
+    detail:     (req, res) => {
+        const product = products.find(p => p.id === parseInt(req.params.id));
+        if (!product) return res.status(404).render('404', { title: 'No encontrado' });
+        res.render('products/detail', { title: `${product.name} – LuBo`, product });
+    },
+    createForm: (req, res) => res.render('products/create', { title: 'Nuevo Producto – LuBo' }),
+    create:     (req, res) => res.redirect('/products'),
+    editForm:   (req, res) => {
+        const product = products.find(p => p.id === parseInt(req.params.id));
+        if (!product) return res.status(404).render('404', { title: 'No encontrado' });
+        res.render('products/edit', { title: `Editar: ${product.name}`, product });
+    },
+    edit:       (req, res) => res.redirect('/products'),
 };
 
 module.exports = productsController;
